e60825f69bc40d85974071c31aef4d809eb029fa
zero <teclaizerai@gmail.com>
2023-08-23 13:46:05 +0200

    Prepare release v0.2.7.

--- src/Module.php
+++ src/Module.php
@@ -9,7 +9,7 @@ class Module
 {
     public const MODULE_NAME = 'Demo Farm add-ons';
 
-    public const MODULE_VERSION = '@dev';
+    public const MODULE_VERSION = 'v0.2.7';
 
     public const MODULE_SOURCE_CODE = 'https://github.com/zerai/oe-module-demo-farm-add-ons';
 
